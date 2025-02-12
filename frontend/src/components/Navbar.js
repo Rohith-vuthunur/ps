7934bd8ba47e75c7a8424dd92fdb4556a0107af4
Rohith-vuthunur <rohithvuthunur@gmail.com>
2025-02-12 22:33:56 +0530

    responsive navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -185,11 +185,64 @@
 //         </nav>
 //     );
 // }
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default function Navbar({ onSearch, onNext }) {
+//     const [searchQuery, setSearchQuery] = useState('');
+
+//     const handleSearch = (e) => {
+//         e.preventDefault();
+//         if (onSearch) {
+//             onSearch(searchQuery);
+//         }
+//     };
+
+//     return (
+//         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//             <div className="container-fluid">
+//                 <Link className="navbar-brand text-white" to="#">PS</Link>
+//                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//                     <span className="navbar-toggler-icon"></span>
+//                 </button>
+//                 <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+//                     <ul className="navbar-nav">
+//                         <li className="nav-item"><Link className="nav-link text-white" to="/">Home</Link></li>
+//                         <li className="nav-item"><Link className="nav-link text-white" to="/ls">Login/Signup</Link></li>
+//                         <li className="nav-item"><Link className="nav-link text-white" to="/lfs">Look For Sponsors</Link></li>
+//                         <li className="nav-item"><Link className="nav-link text-white" to="/lts">Look to Sponsor</Link></li>
+//                         <li className="nav-item"><Link className="nav-link text-white" to="/Showlts">Show Sponsors</Link></li>
+//                     </ul>
+//                     <form className="d-flex ms-auto" onSubmit={handleSearch}>
+//                         <input 
+//                             className="form-control me-2" 
+//                             type="search" 
+//                             placeholder="Search in page..." 
+//                             aria-label="Search"
+//                             value={searchQuery}
+//                             onChange={(e) => setSearchQuery(e.target.value)}
+//                         />
+//                         <button className="btn btn-outline-success" type="submit">Search</button>
+//                         <button className="btn btn-outline-info ms-2" type="button" onClick={onNext}>
+//                             Next
+//                         </button>
+//                     </form>
+//                 </div>
+//             </div>
+//         </nav>
+//     );
+// }
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar({ onSearch, onNext }) {
     const [searchQuery, setSearchQuery] = useState('');
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -198,20 +251,36 @@ export default function Navbar({ onSearch, onNext }) {
         }
     };
 
+    // Toggle navbar collapse
+    const toggleNavbar = () => {
+        setIsNavbarOpen(!isNavbarOpen);
+    };
+
+    // Close navbar when a link is clicked (on mobile)
+    const closeNavbar = () => {
+        setIsNavbarOpen(false);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand text-white" to="#">PS</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    onClick={toggleNavbar} 
+                    aria-expanded={isNavbarOpen} 
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isNavbarOpen ? 'show' : ''}`} id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item"><Link className="nav-link text-white" to="/">Home</Link></li>
-                        <li className="nav-item"><Link className="nav-link text-white" to="/ls">Login/Signup</Link></li>
-                        <li className="nav-item"><Link className="nav-link text-white" to="/lfs">Look For Sponsors</Link></li>
-                        <li className="nav-item"><Link className="nav-link text-white" to="/lts">Look to Sponsor</Link></li>
-                        <li className="nav-item"><Link className="nav-link text-white" to="/Showlts">Show Sponsors</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/" onClick={closeNavbar}>Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/ls" onClick={closeNavbar}>Login/Signup</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/lfs" onClick={closeNavbar}>Look For Sponsors</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/lts" onClick={closeNavbar}>Look to Sponsor</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-white" to="/Showlts" onClick={closeNavbar}>Show Sponsors</Link></li>
                     </ul>
                     <form className="d-flex ms-auto" onSubmit={handleSearch}>
                         <input 
